8ab57aaf8556c80ede7d9ea505d9e0e4fa729d58
Warren Parad <gpg@warrenparad.net>
2022-12-11 19:04:29 +0100

    Remove unnecessary imports.

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const { DateTime } = require('luxon');
 const path = require('path');
-const fs = require('fs-extra');
-const klaw = require('klaw');
-
 
 const directoryPath = path.join(__dirname, `${DateTime.utc().year}`, `day${DateTime.utc().day}.js`);
 require(directoryPath);
